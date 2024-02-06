8e621e283d0db008c84f2d93f4b769cd67cca161
pivner <derykfong@gmail.com>
2024-02-06 10:35:31 -0700

    Fixed calculation of itemoverview

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def main():
     while (r==None):
         r = getter.fetch(input("League:"),getter.filter(input("Item Type:")))
     c = json.loads(r[0].text)
-    flipvalue = [((c["lines"][i]["chaosEquivalent"]-c["lines"][i]["receive"]["value"])) for i in range(len(c["lines"]))] if r[1] else [((c["lines"][i]["chaosValue"])*(c["lines"][i]["sparkline"]["totalChange"]/100)) for i in range(len(c["lines"]))]
+    if r[1]:
+        flipvalue = [((c["lines"][i]["chaosEquivalent"]-c["lines"][i]["receive"]["value"])) for i in range(len(c["lines"]))]
+    else:
+        flipvalue = [(c["lines"][i]["chaosValue"])+(c["lines"][i]["chaosValue"])*(c["lines"][i]["sparkline"]["totalChange"]/100) for i in range(len(c["lines"]))]
     names = [(c["lines"][i]["detailsId"]) for i in range(len(c["lines"]))]
     flipvalue.sort()
     flipvalue.reverse()
