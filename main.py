c57c4f58f15d4a3016b6c79ad160c2de4970baaa
pivner <derykfong@gmail.com>
2024-02-06 10:00:39 -0700

    Made output nicer and added support for itemoverview

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import requests
 import json
 import getter
+import printcolor
 
 def main():
     r = None
     while (r==None):
         r = getter.fetch(input("League:"),getter.filter(input("Item Type:")))
     c = json.loads(r[0].text)
-    flipvalue = [((c["lines"][i]["chaosEquivalent"]-c["lines"][i]["receive"]["value"]),(c["lines"][i]["detailsId"])) for i in range(len(c["lines"]))] if r[1] else [((c["lines"][i]["chaosValue"])*c["lines"][i]["sparkline"]["totalChange"],(c["lines"][i]["detailsId"])) for i in range(len(c["lines"]))]
+    flipvalue = [((c["lines"][i]["chaosEquivalent"]-c["lines"][i]["receive"]["value"])) for i in range(len(c["lines"]))] if r[1] else [((c["lines"][i]["chaosValue"])*(c["lines"][i]["sparkline"]["totalChange"]/100)) for i in range(len(c["lines"]))]
+    names = [(c["lines"][i]["detailsId"]) for i in range(len(c["lines"]))]
     flipvalue.sort()
     flipvalue.reverse()
-    for i in flipvalue:
-        print(i)
+    for i in range(len(flipvalue)):
+        if flipvalue[i]>0:
+            print(printcolor.colors.blue, flipvalue[i],end="")
+        else:
+            print(printcolor.colors.red, flipvalue[i],end="")
+        print(printcolor.colors.pink, names[i],end="\n")
 main()
