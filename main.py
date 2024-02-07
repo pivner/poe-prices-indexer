2bfdc4c39d9215a17214091f45b8c8aaea263551
pivner <derykfong@gmail.com>
2024-02-06 20:12:08 -0700

    Fixed iterating loops

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def main():
         r = getter.fetch(input("League:"),getter.filter(input("Item Type:")))
     c = json.loads(r[0].text)
     if r[1]:
-        flipvalue = [((c["lines"][i]["chaosEquivalent"]-c["lines"][i]["receive"]["value"])) for i in range(len(c["lines"]))]
+        flipvalue = [((l["chaosEquivalent"]-l["receive"]["value"])) for l in c["lines"]]
     else:
-        flipvalue = [(c["lines"][i]["chaosValue"])+(c["lines"][i]["chaosValue"])*(c["lines"][i]["sparkline"]["totalChange"]/100) for i in range(len(c["lines"]))]
+        flipvalue = [(l["chaosValue"])+(l["chaosValue"])*(l["sparkline"]["totalChange"]/100) for l in c["lines"]]
     names = [(c["lines"][i]["detailsId"]) for i in range(len(c["lines"]))]
     flipvalue.sort()
     flipvalue.reverse()
